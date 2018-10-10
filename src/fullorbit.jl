3f9006d2846880ddcf55d9fc91b93a415292a175
Luke Stagner <stagnerl@fusion.gat.com>
2018-10-09 17:03:48 -0700

    add hits_wall for full orbit

--- src/fullorbit.jl
+++ src/fullorbit.jl
@@ -1,4 +1,5 @@
 struct FullOrbitPath{T<:Real}
+    dt::Union{T,Vector{T}}
     r::Vector{T}
     phi::Vector{T}
     z::Vector{T}
@@ -6,6 +7,12 @@ struct FullOrbitPath{T<:Real}
     vphi::Vector{T}
     vz::Vector{T}
 end
+
+function FullOrbitPath(T::DataType=Float64)
+    FullOrbitPath(zero(T),T[],T[],T[],T[],T[],T[])
+end
+
+Base.length(op::FullOrbitPath) = length(op.r)
 
 function borispush(M, v, u, dt)
         x = u[1]
@@ -38,7 +45,10 @@ function borispush(M, v, u, dt)
         return v
 end
 
-function get_full_orbit(M, r, phi, z, vr, vphi, vz; dt= 1e-9,tmax = 100*1e-6)
+function get_full_orbit(M, r, phi, z, vr, vphi, vz; dt= 1e-9, tmax = 100*1e-6)
+
+    tau_c = 2*pi*(mass_u*H2_amu)/(M.b(r,z)*e0)
+
     t = 0:dt:tmax
     nstep = length(t)
     x_arr = zeros(nstep)
@@ -83,7 +93,31 @@ function get_full_orbit(M, r, phi, z, vr, vphi, vz; dt= 1e-9,tmax = 100*1e-6)
     vphi_arr = [-0.5*(vx_arr[i]+vx_arr[i+1])*sp[i] .+ 0.5*(vy_arr[i] + vy_arr[i+1])*cp[i] for i=1:nstep]
     vz_arr = [0.5(vz_arr[i] + vz_arr[i+1]) for i=1:nstep]
 
-    return FullOrbitPath(r_arr,phi_arr,z_arr,vr_arr,vphi_arr,vz_arr)
+    return FullOrbitPath(dt, r_arr,phi_arr,z_arr,vr_arr,vphi_arr,vz_arr)
 end
 
+function hits_wall(M, r, phi, z, vr, vphi, vz, wall; dt=1e-9, tmax=100e-6)
+    t = 0:dt:tmax
+    nstep = length(t)
 
+    sp = sin(phi)
+    cp = cos(phi)
+    u = SVector{3}([r*cp,r*sp,z])
+    v = SVector{3}([vr*cp - vphi*sp,vr*sp + vphi*cp,vz])
+    hit = false
+    v = borispush(M,v,u,-0.5*dt)
+    T = 0.0
+    for i=1:nstep
+        v = borispush(M,v,u,dt)
+        u = u .+ v*dt
+        rr = sqrt(u[1]^2 + u[2]^2)
+        zz = u[3]
+        if ~in_vessel(wall,(rr,zz))
+            hit = true
+            break
+        end
+        T += dt
+    end
+
+    return hit, T
+end
