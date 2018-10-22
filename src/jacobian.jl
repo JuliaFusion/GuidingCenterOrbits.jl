179fad17bce222841407f7c2c2303bbec749eb2a
Luke Stagner <stagnerl@fusion.gat.com>
2018-10-22 15:02:28 -0700

    zero jacobian for lost/incomplete orbits

--- src/jacobian.jl
+++ src/jacobian.jl
@@ -1,9 +1,12 @@
 function eprz_to_eprt(M, energy, pitch, r, z; m=H2_amu, q=1, kwargs...)
 
+    islost=[false]
+    z = 
     f = function c(x; kwargs...)
         gcp = GCParticle(x[1], x[2], x[3], x[4], H2_amu*mass_u, q)
         path, stat = integrate(M, gcp; store_path=false, one_transit=true, kwargs...)
         if stat.class in (:lost,:incomplete) || stat.errcode == 1
+            islost[1] = true
             return x
         end
         hc = HamiltonianCoordinate(M, gcp)
@@ -18,7 +21,11 @@ function eprz_to_eprt(M, energy, pitch, r, z; m=H2_amu, q=1, kwargs...)
     ForwardDiff.jacobian!(jr, x->f(x; kwargs...), x, jcfg, Val{false}())
 
     v = DiffResults.value(jr)
-    detJ = abs(det(DiffResults.jacobian(jr)))
+    if islost[1]
+        detJ = 0.0
+    else
+        detJ = abs(det(DiffResults.jacobian(jr)))
+    end
 
     return v, detJ
 end
