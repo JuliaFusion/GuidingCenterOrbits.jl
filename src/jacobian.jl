af2b02ed59c2091b6d55b124de63c5b5299e6e62
Luke Stagner <lstagner@uci.edu>
2019-01-14 22:03:46 -0800

    add eprt_to_eprz

--- src/jacobian.jl
+++ src/jacobian.jl
@@ -50,15 +50,64 @@ function eprz_to_eprt(M, energy, pitch, r, z; m=H2_amu, q=1, auto_diff = true, k
     return v, detJ
 end
 
-function get_orbit_jacobian(M::AxisymmetricEquilibrium, c::EPRCoordinate; kwargs...)
+function eprt_to_eprz(M, energy, pitch, r, t; tau_p, m=H2_amu, q=1, auto_diff = true, kwargs...)
+    islost=[false]
+    erred = [true]
+    f = function c(x; kwargs...)
+        erred[1] = false
+        islost[1] = false
+        gcp = EPRCoordinate(M,x[1], x[2], x[3], amu=m, q=q)
+        path, stat = integrate(M, gcp; tmax = x[4]*tau_p, interp_dt=0, store_path=true, classify_orbit=false, one_transit=false, kwargs...)
+        if stat.class == :lost
+            islost[1] = true
+            return x
+        elseif stat.errcode == 1
+            erred[1] = true
+            return x
+        end
+        return [path.energy[end], path.pitch[end], path.r[end], path.z[end]]
+    end
+
+    x = [energy,pitch,r,t]
+
+    if auto_diff
+        jr = DiffResults.JacobianResult(x)
+        jcfg = ForwardDiff.JacobianConfig(nothing, x)
+        ForwardDiff.jacobian!(jr, x->f(x; kwargs...), x, jcfg, Val{false}())
+
+        v = DiffResults.value(jr)
+        if !erred[1]
+            if islost[1]
+                detJ = 0.0
+            else
+                detJ = abs(det(DiffResults.jacobian(jr)))
+            end
+        else
+            detJ = 0.0
+        end
+    end
+    if erred[1] && !auto_diff
+        v = f(x; kwargs...)
+        if islost[1] || erred[1]
+            detJ = 0.0
+        else
+            J = DiffEqDiffTools.finite_difference_jacobian(x->f(x;kwargs...), x,
+                                                           Val{:central}, eltype(x), Val{false})
+            detJ = abs(det(J))
+        end
+    end
+    return v, detJ
+end
+
+function get_orbit_jacobian(M::AxisymmetricEquilibrium, c::EPRCoordinate; tau_p=1.0, kwargs...)
     ed = ForwardDiff.Dual(c.energy,(1.0,0.0,0.0,0.0))
     pd = ForwardDiff.Dual(c.pitch, (0.0,1.0,0.0,0.0))
     rd = ForwardDiff.Dual(c.r,     (0.0,0.0,1.0,0.0))
-    td = ForwardDiff.Dual(0.0,     (0.0,0.0,0.0,1.0))
+    td = ForwardDiff.Dual(1.0,     (0.0,0.0,0.0,1.0))
     zd = one(td)*c.z
-    cdual = EPRCoordinate(ed,pd,rd,zd,c.m,c.q)
-    path, stat = integrate(M, cdual; tmin = td, one_transit = true, kwargs...)
-
+    cdual = EPRCoordinate(M,ed,pd,rd)
+    path, stat = integrate(M, cdual; one_transit = true, kwargs...)
+    return path
     np = length(path)
     detJ = zeros(np)
     for i=1:np
